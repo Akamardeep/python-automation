25b5a56752b0ae2d8f8fcfd429551b8828b596e5
AMARDEEP KINI <92258265+Akamardeep@users.noreply.github.com>
2024-04-23 11:10:58 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -18,8 +18,8 @@ do
           aws elbv2 deregister-targets --target-group-arn $target_group_arn --targets Id=$instance_id
           # HEALTH_STATUS=$(aws elbv2 describe-target-health --target-group-arn $TARGET_GROUP_ARN --targets Id=$instance_id | jq -r '.TargetHealthDescriptions[0].TargetHealth.State')
           # echo "$HEALTH_STATUS"
-          MAX_Deregistration_Retries : ${{ variables.MAX_Deregistration_Retries }}
-          MAX_Deregistration_Retries :${{variables.MAX_Deregistration_Retries
+           MAX_DEREGISTRATION_RETRIES: ${{ env.ENV_DEV_MAX_DEREGISTRATION_RETRIES}}
+           DEREGISTRATION_WAIT_TIME: ${{ env.ENV_DEV_ DEREGISTRATION_WAIT_TIME }}
           retry_count=0
           while [ $retry_count -lt $MAX_RETRIES ]; do
            HEALTH_STATUS=$(aws elbv2 describe-target-health --target-group-arn $target_group_arn --targets Id=$instance_id | jq -r '.TargetHealthDescriptions[0].TargetHealth.State')
@@ -61,8 +61,8 @@ EOF
        sleep 5
        echo "out of ec2 instance"
        aws elbv2 register-targets --target-group-arn $target_group_arn --targets Id=$instance_id
-       REGISTER_MAX_RETRIES=10
-       REGISTER_WAIT_TIME=10
+       MAX_REGISTRATION_RETRIES: ${{ env.ENV_DEV_MAX_DEREGISTRATION_RETRIES}}
+       REGISTRATION_WAIT_TIME: ${{ env.ENV_DEV_ DEREGISTRATION_WAIT_TIME }}
        register_retry_count=0
         while [ $register_retry_count -lt $REGISTER_MAX_RETRIES ]; do
         REGISTER_HEALTH_STATUS=$(aws elbv2 describe-target-health --target-group-arn $target_group_arn --targets Id=$instance_id | jq -r '.TargetHealthDescriptions[0].TargetHealth.State')
