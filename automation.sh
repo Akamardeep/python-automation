5c98b9fcfdfd586d7ba4cf556279715fdaf1391c
AMARDEEP KINI <92258265+Akamardeep@users.noreply.github.com>
2024-06-17 15:03:29 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install -y awscli
 echo "describe passed"
   
-TARGET_GROUP_ARN='arn:aws:elasticloadbalancing:eu-west-1:508308164161:targetgroup/automation-alb-tg/28f6ebf3091af7af arn:aws:elasticloadbalancing:eu-west-1:508308164161:targetgroup/automation-alb-tg-2/1da2b6af3ebcd5cf'
+TARGET_GROUP_ARN='arn:aws:elasticloadbalancing:us-west-2:628725545865:targetgroup/demo-1-tg/1523c8d8f7c133c5'
 echo "target group arn is :$TARGET_GROUP_ARN"
  INSTANCE_COUNT=0
   
